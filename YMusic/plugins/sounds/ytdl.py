413320f656a876108faf2b0e0c42defb92f53e92
Botz Hub <botzhub01@gmail.com>
2025-05-17 21:08:28 +0530

    Update ytdl.py

--- YMusic/plugins/sounds/ytdl.py
+++ YMusic/plugins/sounds/ytdl.py
@@ -1,22 +1,18 @@
 import os
 import time
 from io import BytesIO
-
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
-from utils.scripts import import_library
-
-yt_dlp = import_library("yt_dlp", "yt-dlp")
-
 from urllib.parse import parse_qs, urlparse
-
 from yt_dlp import YoutubeDL
 from yt_dlp.utils import DownloadError, ExtractorError
 
-from utils.misc import modules_help, prefix
-from utils.scripts import format_exc, progress, resize_image
+from YMusic import app
+from config import PREFIXES
+from YMusic.utils.decorators import errors, admins_only
+from YMusic.utils.filters import command
+from YMusic.utils.helpers import progress, resize_image, format_exc
 
 ydv_opts = {
     "format": "bv[ext=mp4]+ba/bv*+ba/b[ext=mp4]/b",
@@ -39,7 +35,6 @@ ydm_opts = {
 
 API_URL = "https://apis.davidcyriltech.my.id/"
 
-
 def extract_video_id(url):
     parsed_url = urlparse(url)
     if parsed_url.hostname == "youtu.be":
@@ -48,7 +43,6 @@ def extract_video_id(url):
         query_params = parse_qs(parsed_url.query)
         video_id = query_params.get("v", [None])[0]
     return video_id
-
 
 def search_api(query, is_videoId=False, video=False):
     query = str(query)
@@ -79,7 +73,6 @@ def search_api(query, is_videoId=False, video=False):
                 return title, thumb_url, link
     return None, None, None
 
-
 def download_video(url):
     try:
         with YoutubeDL(ydv_opts) as ydl:
@@ -88,7 +81,6 @@ def download_video(url):
             title = info.get("title")
             ext = info.get("ext")
             thumb_url = info.get("thumbnail")
-            print(thumb_url)
             img = None
             if thumb_url:
                 resp = requests.get(thumb_url)
@@ -116,13 +108,11 @@ def download_video(url):
             f.write(resp.content)
         return "downloads/ytdl/videos/" + title + ".mp4", title, img, thumb_url
 
-
 def download_music(url):
     try:
         with YoutubeDL(ydm_opts) as ydl:
             info = ydl.extract_info(url, download=False)
             thumb_url = info.get("thumbnail")
-            print(thumb_url)
             img = None
             if thumb_url:
                 resp = requests.get(thumb_url)
@@ -130,7 +120,6 @@ def download_music(url):
                     f.write(resp.content)
                 img = resize_image("thumbyt.jpg")
             ydl.download([url])
-            # Search for the file in the audios folder
             for file in os.listdir("downloads/ytdl/audios"):
                 file_path = os.path.join("downloads/ytdl/audios", file)
                 title = os.path.splitext(file)[0]
@@ -156,9 +145,10 @@ def download_music(url):
             f.write(resp.content)
         return "downloads/ytdl/audios/" + title + ".mp3", title, img
 
-
-@Client.on_message(filters.command(["ytv", "ytm"], prefix) & filters.me)
-async def ytvm(client: Client, message: Message):
+@app.on_message(command(["ytv", "ytm"], PREFIXES) & filters.me)
+@errors
+@admins_only
+async def yt_downloader(client: Client, message: Message):
     if len(message.command) < 2:
         return await message.reply_text("Please provide a YouTube URL to download.")
 
@@ -166,7 +156,6 @@ async def ytvm(client: Client, message: Message):
         await message.edit_text("Starting Download...")
         try:
             url = message.text.split(maxsplit=1)[1]
-            # await message.edit_text("Starting Download...")
             file_path, title, img, thumb_url = download_video(url)
 
             new_path = file_path.replace(".webm", ".mp4")
@@ -180,13 +169,13 @@ async def ytvm(client: Client, message: Message):
                 message.chat.id,
                 video=file_path,
                 thumb=img,
-                cover=BytesIO(requests.get(thumb_url).content) if thumb_url else None,
                 caption=f"<code>{title}</code>",
                 progress=progress,
                 progress_args=(ms, c, "<code>Uploading Video...</code>"),
             )
             os.remove(file_path)
-            os.remove("thumbyt.jpg")
+            if os.path.exists("thumbyt.jpg"):
+                os.remove("thumbyt.jpg")
             await message.delete()
         except Exception as e:
             await message.edit_text(f"An error occurred: {format_exc(e)}")
@@ -212,15 +201,8 @@ async def ytvm(client: Client, message: Message):
                 progress_args=(ms, c, "<code>Uploading Audio...</code>"),
             )
             os.remove(file_path)
-            os.remove("thumbyt.jpg") if os.path.exists("thumbyt.jpg") else None
+            if os.path.exists("thumbyt.jpg"):
+                os.remove("thumbyt.jpg")
             await message.delete()
         except Exception as e:
             await message.edit_text(f"An error occurred: {format_exc(e)}")
-    else:
-        return await message.edit_text("Oh Damn Lol")
-
-
-modules_help["ytdl"] = {
-    "ytv [name|link]*": "Download Video From YouTube",
-    "ytm [name|link]*": "Download Music From YouTube",
-}
