d97035a9a0c3847c5862e52bc1086cce92048f58
Botz Hub <botzhub01@gmail.com>
2025-05-17 21:04:04 +0530

    Delete YMusic/utils/misc.py

--- YMusic/utils/misc.py
+++
@@ -1,9 +0,0 @@
-modules_help = {}  # Dictionary to store help info for modules
-prefix = "."       # Default command prefix
-
-def resize_image(image_path):
-    """Basic image resizing function"""
-    from PIL import Image
-    img = Image.open(image_path)
-    img.thumbnail((320, 320))
-    return img
